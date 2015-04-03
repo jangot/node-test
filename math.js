1edd7408f042b1e679c6380333436d318c3ee10c
jangot <pulin.pavel@gmail.com>
2015-04-03 13:42:23 +0300

    sql

--- math.js
+++ math.js
@@ -4,10 +4,9 @@ var factorial = exports.factorial = function(n) {
     } else {
         return n * factorial(n - 1);
     }
-}
+};
 
 var fibonacci = exports.fibonacci = function(n) {
-    console.log(n);
     if (n === 1) {
         return 1;
     } else if (n === 2) {
@@ -15,4 +14,20 @@ var fibonacci = exports.fibonacci = function(n) {
     } else {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
-}
+};
+
+var fibonacciAsync = exports.fibonacciAsync = function(n, done) {
+    if (n === 1 || n === 2) {
+        done(1);
+    } else {
+        setImmediate(function() {
+            fibonacciAsync(n-1, function(val1) {
+                setImmediate(function() {
+                    fibonacciAsync(n-2, function(val2) {
+                        done(val1 + val2);
+                    })
+                });
+            });
+        });
+    }
+};
